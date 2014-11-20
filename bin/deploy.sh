fbd4bb3d7d8c1d28213b1dbcf5400554e8ec3551
Sem <semia.ladjal@gmail.com>
2014-11-20 16:07:18 +0100

    echo added in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,10 +4,10 @@ DIRECTION="git@heroku.com:highway-to-fablab.git"
 
 echo "Deploiement sur heroku"
 
-git pull origin master && \
-bundle exec rake && \
-git push origin master && \
-git push $DIRECTION master && \
-heroku run rake db:migrate -a highwaytofablab
+git pull origin master && \ echo "pull OK"
+bundle exec rake && \ echo "bundle exec rake && OK"
+git push origin master && \ echo "push on origin master OK"
+git push $DIRECTION master && \ echo "push on $DIRECTION master OK"
+heroku run rake db:migrate -a highwaytofablab echo "data base migration OK"
 
 echo "********** OK **********"
